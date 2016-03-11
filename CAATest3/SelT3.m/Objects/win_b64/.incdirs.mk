0d7b3ab51832671328d478a7111953fccbb1225a
panhao <4812ph@163.com>
2016-03-11 09:38:42 +0800

    20160311

--- CAATest3/SelT3.m/Objects/win_b64/.incdirs.mk
+++
@@ -1,5 +0,0 @@
-/I"D:\catiav5_r22\TestSpace\.\CAATest3\SelT3.m\src"
-/I"D:\catiav5_r22\TestSpace\.\CAATest3\SelT3.m\LocalInterfaces"
-/I"D:\catiav5_r22\TestSpace\.\CAATest3\SelT3.m\LocalGenerated\win_b64"
-/I"D:\catiav5_r22\TestSpace\.\CAATest3\PublicInterfaces"
-/I"D:\catiav5_r22\TestSpace\.\CAATest3\ImportedInterfaces\win_b64"
